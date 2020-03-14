0f477bf17875f5a422ffaefaa8c3290f6de6b51f
nicholashobbs <3465766+nicholashobbs@users.noreply.github.com>
2020-03-13 21:12:07 -0600

    change name, update scripts

---
+++ GuidesDemo/make_ents.py
@@ -0,0 +1,49 @@
+import ruamel.yaml as yaml
+import openlattice
+from olpy.flight import Flight
+
+jwt = input('Input your JWT:')
+
+fl = Flight()
+fl.deserialize("guides_demo.yaml")
+
+configuration = openlattice.Configuration()
+configuration.host = "https://api.openlattice.com"
+configuration.api_key_prefix['Authorization'] = 'Bearer'
+configuration.api_key['Authorization'] = jwt
+
+edm_api = openlattice.EdmApi(openlattice.ApiClient(configuration))
+
+lbfd = fl.schema
+
+def make_ents_from_flight(fd, orgid):
+    created_ents = []
+    for defn in fd:
+        for ent in fd[defn]:
+        
+            nm = fd[defn][ent]['fqn'].split('.')
+            
+            out = {
+                "entityTypeId": edm_api.get_entity_type_id(namespace=nm[0],name=nm[1]),
+                "name": fd[defn][ent]['entity_set_name'],
+                "title": fd[defn][ent]['entity_set_name'],
+                "contacts": ["nicholas@openlattice.com"],
+                "organizationId": orgid
+                
+            }
+            created_ents.append(out)
+            
+    return created_ents
+
+entds = make_ents_from_flight(lbfd, '1d5aa1f4-4d22-46a5-97cd-dcc6820e7ff8')
+
+def create_entity_sets(entlist):
+    for entity in entlist:
+        try:
+            edm_api.create_entity_sets([entity]) 
+            print(f"Created {entity['name']}\n\n")
+        except openlattice.rest.ApiException as exc:
+            print(exc)
+
+
+create_entity_sets(entds)
